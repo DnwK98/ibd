fab03eb6ec0f7fc804133bdd378fbb93fa5d7b41
Damian <damian022w@wp.pl>
2021-04-17 13:18:47 +0200

    [LAB-3] Dodanie do stronicowania linkow kierujacych do nastepnej, poprzedniek, pierwszei i ostatniej strony

--- src/Stronicowanie.php
+++ src/Stronicowanie.php
@@ -71,10 +71,21 @@ class Stronicowanie
     public function pobierzLinki(string $select, string $plik): string
     {
         $rekordow = $this->db->policzRekordy($select, $this->parametryZapytania);
-        $liczbaStron = ceil($rekordow / $this->naStronie);
+        $liczbaStron = (int)ceil($rekordow / $this->naStronie);
         $parametry = $this->_przetworzParametry();
 
         $linki = "<nav><ul class='pagination'>";
+        $linki .= sprintf("<li class='page-item %s'><a href='%s?%s&strona=0' class='page-link'>Początek</a></li>",
+            $this->strona === 0 ? 'disabled' : '',
+            $plik,
+            $parametry
+        );
+        $linki .= sprintf("<li class='page-item %s'><a href='%s?%s&strona=%s' class='page-link'>Poprzednia</a></li>",
+            $this->strona === 0 ? 'disabled' : '',
+            $plik,
+            $parametry,
+            $this->strona - 1
+        );
         for ($i = 0; $i < $liczbaStron; $i++) {
             if ($i == $this->strona) {
                 $linki .= sprintf("<li class='page-item active'><a class='page-link'>%d</a></li>", $i + 1);
@@ -88,6 +99,19 @@ class Stronicowanie
                 );
             }
         }
+        $linki .= sprintf("<li class='page-item %s'><a href='%s?%s&strona=%s' class='page-link'>Następna</a></li>",
+            $this->strona === $liczbaStron - 1 ? 'disabled' : '',
+            $plik,
+            $parametry,
+            $this->strona + 1
+        );
+
+        $linki .= sprintf("<li class='page-item %s'><a href='%s?%s&strona=%s' class='page-link'>Koniec</a></li>",
+            $this->strona === $liczbaStron - 1 ? 'disabled' : '',
+            $plik,
+            $parametry,
+            $liczbaStron - 1,
+        );
         $linki .= "</ul></nav>";
 
         return $linki;
